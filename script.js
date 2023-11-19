ec141bac0d19306d3b53ba98bfa6dab689f7a3a6
matiamorfati <121320678+matiamorfati@users.noreply.github.com>
2023-11-19 15:56:42 +0100

    make createBoard function to split board to divs

---
+++ script.js
@@ -0,0 +1,19 @@
+document.addEventListener("DOMContentLoaded", function(){
+    createBoard(8);
+});
+
+
+function createBoard(size){
+let board = document.querySelector(".board");
+board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+let numDivs = size * size;
+
+for(let i = 0; i < numDivs; i++){
+    let div = document.createElement("div");
+    board.insertAdjacentElement('beforeend', div);
+}
+
+
+}
