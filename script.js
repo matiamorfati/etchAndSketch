32d125c07ef7f55a1738e2648e3d6e366b6330b5
matiamorfati <121320678+matiamorfati@users.noreply.github.com>
2023-11-22 15:46:00 +0100

    make size button work

--- script.js
+++ script.js
@@ -1,19 +1,40 @@
 document.addEventListener("DOMContentLoaded", function(){
     createBoard(8);
+
+    let btn_popup = document.querySelector("#popup");
+    btn_popup.addEventListener("click", function(){
+        let size = getSize();
+        createBoard(size);
+    });
+    
 });
 
 
 function createBoard(size){
-let board = document.querySelector(".board");
-board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+    let board = document.querySelector(".board");
+    board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
 
-let numDivs = size * size;
+    let numDivs = size * size;
 
-for(let i = 0; i < numDivs; i++){
-    let div = document.createElement("div");
-    board.insertAdjacentElement('beforeend', div);
+    for(let i = 0; i < numDivs; i++){
+        let div = document.createElement("div");
+        board.insertAdjacentElement('beforeend', div);
+    }
 }
 
+function getSize(){
+    let input = window.prompt("What size of the board would you like Sir?");
 
+    let message = document.querySelector("#message");
+    if(isNaN(input)){
+        message.innerHTML = "Provide a number";
+    } 
+    else if(input < 1 || input > 100){
+        message.innerHTML = "Provide a number between 1 and 100";
+    }
+    else{
+        message.innerHTML = "now you can play";
+        return input;
+    }
 }
